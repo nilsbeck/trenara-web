ba7d05b2c47eedc6a74c937d23ad23db724cb441
Nils Beckmann <nils.beckmann@gmail.com>
2025-02-24 15:45:40 +0100

    set goal min/max width

--- src/lib/components/goal.svelte
+++ src/lib/components/goal.svelte
@@ -22,7 +22,7 @@
 	});
 </script>
 
-<div class="card dark:bg-gray-700 bg-gray-50 shadow-lg max-w-sm w-full flex-grow">
+<div class="card dark:bg-gray-700 bg-gray-50 shadow-lg max-w-sm min-w-sm w-full flex-grow">
     <div class="card-body">
         <h2 class="card-title">Event: {goal.name}</h2>
         <p>Distance: {goal.distance}</p>
