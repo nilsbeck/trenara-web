208cc239b3827d2950a6dde1cd4dab8ed6910bd8
Nils Beckmann <nils.beckmann@gmail.com>
2025-03-27 10:41:46 +0100

    refactoring loading

--- src/lib/components/loading.svelte
+++ src/lib/components/loading.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	let { text = "Loading..." }: { text?: string } = $props();
+</script>
+
 <div class="flex items-center justify-center">
 	<svg
 		class="mr-3 -ml-1 size-5 animate-spin"
@@ -13,5 +17,5 @@
 		d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
 	></path></svg
 >
-	<p>Loading...</p>
+	<p>{text}</p>
 </div>
