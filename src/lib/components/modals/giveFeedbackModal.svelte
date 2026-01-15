e8f86352a46943734e949aac94d4d5cd822f8770
Nils Beckmann <nils.beckmann@gmail.com>
2026-01-15 16:18:12 +0100

    add loading animation to save feedback dialog

--- src/lib/components/modals/giveFeedbackModal.svelte
+++ src/lib/components/modals/giveFeedbackModal.svelte
@@ -4,6 +4,7 @@
 	import type { ScheduledTraining, Entry } from '$lib/server/api/types';
 
 	let giveFeedbackModal: HTMLDialogElement = $state() as HTMLDialogElement;
+	let saveFeedbackLoading: boolean = $state(false);
 
 	let {
 		selectedTraining,
@@ -106,15 +107,45 @@
 								<button
 									class="btn btn-primary"
 									type="button"
+									disabled={saveFeedbackLoading}
 									onclick={async () => {
+										saveFeedbackLoading = true;
 										await postFeedback(selectedRunTrainingEntry[0].id, feedbackInitValue)
 											.then(() => {
 												selectedRunTrainingEntry[0].rpe = feedbackInitValue;
 												giveFeedbackModal.close();
 											})
-											.catch((error) => alert(`Failed to save feedback: ${error.error}`));
-									}}>Save</button
+											.catch((error) => alert(`Failed to save feedback: ${error.error}`))
+											.finally(() => {
+												saveFeedbackLoading = false;
+											});
+									}}
 								>
+									{#if saveFeedbackLoading}
+										<svg
+											class="mr-3 -ml-1 size-5 animate-spin text-white"
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+										>
+											<circle
+												class="opacity-25"
+												cx="12"
+												cy="12"
+												r="10"
+												stroke="currentColor"
+												stroke-width="4"
+											></circle>
+											<path
+												class="opacity-75"
+												fill="currentColor"
+												d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+											></path>
+										</svg>
+									{:else}
+										Save
+									{/if}
+								</button>
 							</div>
 						</fieldset>
 					</form>
